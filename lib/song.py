934c924ca85ddd01b4c331ade8f93c92d6190645
Eugene Kimathi <eugenekimathi@gmail.com>
2026-06-10 12:14:27 +0300

    Implemented a Song class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,63 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count ={}
+    artists_count ={}
+
+    def __init__(self,name,artist,genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artist(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artists_count(artist)
+
+    @classmethod
+    def add_song_to_count (cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls,genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artist(cls,artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls,genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls,artist):
+        if artist in cls.artists_count:
+            cls.artists_count[artist] += 1
+        else:
+            cls.artists_count[artist] = 1
+
+    @classmethod
+    def show_all_songs_info(cls):
+        return {
+            "Total Songs": cls.count,
+            "Artists":cls.artists,
+            "Genre Count":cls.genre_count,
+            "Artists Count":cls.artists_count
+        }
+
+
+song1 = Song("Halo", "Beyonce", "Pop")
+song2 = Song("99 Problems", "Jay-Z", "Rap")
+song3 = Song("Formation", "Beyonce", "Rap")
+song4 = Song ("Proving","Popcaan","Dancehall")
+
+print(Song.show_all_songs_info())
+
